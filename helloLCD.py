956894ab6b6532ca00f1123f775572b625f370e9
dwill193 <rmdean96@yahoo.com>
2018-03-19 18:13:38 +0000

    TUFF

--- helloLCD.py
+++ helloLCD.py
@@ -1,15 +1,47 @@
 #!/usr/bin/python
-#show "Hello World LCD screen"
+#show IP address on the LCD Plate at startup
 #Meandean
 
 import Adafruit_CharLCD as LCD
-
+import time
+import subprocess
+  # lint:ok
 
 lcd = LCD.Adafruit_CharLCDPlate()
 
+Name = subprocess.check_output("hostname").strip()
+displayText = Name
+  # lint:ok
 
-displayText = "Hello World"
+IP = subprocess.check_output(["hostname", "-I"])
+refresh = True
 
-lcd.clear()
-lcd.message(displayText)
+
+while(True):
+    if lcd.is_pressed(LCD.SELECT):
+        lcd.clear()
+        lcd.message(displayText + "\n")
+        lcd.set_backlight(1)
+        lcd.message("Hello World\n")
+        refresh = True
+
+    else:
+        if refresh:
+            lcd.clear()
+            lcd.set_backlight(1)
+            lcd.message(displayText + "\n")
+            lcd.message(IP)
+            refresh = False
+    time.sleep(0.5)
+
+
+
+
+
+
+
+
+
+
+
 
